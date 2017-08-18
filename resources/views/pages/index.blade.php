031e3dec9d9c3cbd3b08824d00e4b984d302a30e
johnmorris4 <johnmorris4@bellsouth.net>
2017-08-18 05:36:12 -0400

    Save work after app crash

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+
+
+    <h1>{{$title}}</h1>
+    {{--  <<h1>Welcome to the Laravel Application</h1>  --}}
+    <p>This is a Laravel application that was built from "The Laravel from Scratch" series.</p>
+    <p>Have a look around and please share if you find this series intesting</p>
+
+@endsection
